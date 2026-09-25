8f0c32f588a7c2d6bb847266ec8da02416053eb9
maximpsn <pavlusenko.maksim@yandex.ru>
2026-09-25 09:06:08 +0300

    Митигация: компенсация 40px плашки Layero (min-height calc(100vh-40px))

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
 import PageTransition from './components/router/PageTransition'
 
 function App() {
+  useEffect(() => {
+    if (document.querySelector('layero-badge')) {
+      document.documentElement.classList.add('has-layero-footer')
+    }
+  }, [])
+
   return <PageTransition />
 }
 
