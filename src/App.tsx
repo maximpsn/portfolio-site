1c013513adb7fc1bef3320b4166adacc1249fe71
maximpsn <pavlusenko.maksim@yandex.ru>
2026-09-25 09:07:54 +0300

    Revert митигации плашки Layero (не помогло)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { useEffect } from 'react'
 import PageTransition from './components/router/PageTransition'
 
 function App() {
-  useEffect(() => {
-    if (document.querySelector('layero-badge')) {
-      document.documentElement.classList.add('has-layero-footer')
-    }
-  }, [])
-
   return <PageTransition />
 }
 
